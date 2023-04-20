b94a72bdc42cbfe35a3377d9b851c2bc1e351d25
Sebastian <sebastian.danielsson@proton.me>
2023-04-20 09:57:09 +0200

    Fix task 3

--- index.js
+++ index.js
@@ -3,119 +3,25 @@ var mymap = L.map('map').setView([60.484296, 15.417776], 11);
 var lyrOSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 mymap.addLayer(lyrOSM);
 
-var ctlZoomslider = L.control.zoomslider({position: 'topright'}).addTo(mymap);
-
 var sidebar = L.control.sidebar('sidebar', {position: 'left'});
 mymap.addControl(sidebar);
 
 var polylineMeasure = L.control.polylineMeasure({
-  position: 'topleft',
-  unit: 'kilometres',
+  clearMeasurementsOnStop: false,
+  showClearControl: true,
+  showUnitControl: true,
 });
 polylineMeasure.addTo(mymap);
-
-const line1coords = [
-  {lat: 60.484296, lng: 15.417776},
-  {lat: 60.513259, lng: 15.386245},
-  {lat: 60.497992, lng: 15.406083},
-  {lat: 60.494956, lng: 15.434257},
-  {lat: 60.482664, lng: 15.4482367},
-];
-const line2coords = [
-  {lat: 19.880391767822505, lng: -159.67529296875003},
-  {lat: 17.90556881196468, lng: -156.39038085937503},
-];
-
-polylineMeasure.seed([line1coords, line2coords]);
-
-// Task 3 - Supermarkets
-
-/* var sample2 = L.geoJson(superMarkets, {
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup(feature.properties.name);
-    },
-  });
-
-  sample2.addTo(mymap);
-  var s2 = sample2.toGeoJSON();
-  var samplegeo = turf.buffer(s2, 1, { units: "kilometers" });
-  var samplegeol = L.geoJSON(samplegeo, {
-    style: { color: "yellow", dashArray: "5,5", fillOpacity: 0.1 },
-  }).addTo(mymap); */
-
-var superMarket = superMarkets;
-let tempMarkets = [];
-let tempMarket = [];
-let overLapMarkets = [];
-
-var minimumDistance = 1000;
-var markets = L.geoJson(superMarket, {
-  onEachFeature: function (featureData) {
-    var circle = L.circle([featureData.geometry.coordinates[1], featureData.geometry.coordinates[0]], {
-      color: '#add8e6',
-      fillColor: '#add8e6',
-      fillOpacity: 0.5,
-      radius: 500,
-    }).addTo(mymap);
-    tempMarkets.push([
-      featureData.properties.full_id,
-      featureData.properties.name,
-      featureData.geometry.coordinates[1],
-      featureData.geometry.coordinates[0],
-    ]);
-  },
-}).addTo(mymap);
-
-var overlap = L.geoJson(superMarket, {
-  onEachFeature: function (featureData, featureLayer) {
-    tempMarket = [];
-    tempMarkets.forEach(
-      (item) => {
-        let distance1 = L.latLng(item[2], item[3]);
-        let distance2 = L.latLng(featureData.geometry.coordinates[1], featureData.geometry.coordinates[0]);
-        let distance3 = distance1.distanceTo(distance2);
-        if (distance3 <= minimumDistance && distance3 !== 0) {
-          tempMarket.push(item[1]);
-          var circle = L.circle([featureData.geometry.coordinates[1], featureData.geometry.coordinates[0]], {
-            color: 'red',
-            fillColor: '#000',
-            fillOpacity: 0.25,
-            radius: 500,
-          }).addTo(mymap);
-        }
-      },
-      overLapMarkets.push({
-        key: featureData.properties.full_id,
-        value: tempMarket,
-      }),
-      featureLayer
-        .on('click', function () {
-          Object.keys(overLapMarkets).forEach((key) => {
-            if (overLapMarkets[key].key == featureData.properties.full_id) {
-              overlapValue = overLapMarkets[key].value;
-              sidebar.setContent('Store info <br>Store Name: ' + featureData.properties.name + '<br/>overlaps: ' + overlapValue);
-              sidebar.show();
-            }
-          });
-        })
-        .addTo(mymap),
-    );
-  },
-});
-
-document.getElementById('btnTask3').addEventListener('click', function () {
-  mymap.setView([60.30129385267935, 18.410410081739627], 12);
-});
 
 // Task 1 - Create a red polygon
 var blackIcon = L.icon({
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/svg/map-marker.svg',
-  iconSize: [32, 37], // size of the icon
-  iconAnchor: [32, 37], // point of the icon which will correspond to marker's location
-  popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
+  iconSize: [32, 37],
+  iconAnchor: [32, 37],
+  popupAnchor: [0, -30],
 });
 
-telia = L.marker([60.60678124412115, 15.62869672216047], {
+L.marker([60.60678124412115, 15.62869672216047], {
   icon: blackIcon,
 })
   .addTo(mymap)
@@ -241,6 +147,16 @@ icaMaxi = L.marker([60.482664, 15.4482367], {
     sidebar.show();
   });
 
+const task2Cords = [
+  {lat: 60.482664, lng: 15.4482367},
+  {lat: 60.484296, lng: 15.417776},
+  {lat: 60.494956, lng: 15.434257},
+  {lat: 60.497992, lng: 15.406083},
+  {lat: 60.513259, lng: 15.386245},
+];
+
+polylineMeasure.seed([task2Cords]);
+
 $('#btnTask2').click(function () {
   mymap.setView([60.484296, 15.417776], 12);
   removeIkea();
@@ -250,6 +166,45 @@ $('#btnTask2').click(function () {
         <p style='font-size: 12px;'>Klicka på en punkt för mer information.</p>`,
   );
   sidebar.show();
+});
+
+// Task 3 - Supermarkets
+
+var layer = L.geoJson(superMarkets, {
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.name);
+  },
+});
+
+layer.addTo(mymap);
+var supermarkets = layer.toGeoJSON();
+var buffers = turf.buffer(supermarkets, 1, {units: 'kilometers'});
+
+function buffersOverlap(feature1, feature2) {
+  return turf.booleanIntersects(feature1, feature2);
+}
+
+function styleBuffers(feature) {
+  let isOverlapping = false;
+
+  buffers.features.forEach((otherFeature) => {
+    if (feature !== otherFeature && buffersOverlap(feature, otherFeature)) {
+      isOverlapping = true;
+    }
+  });
+
+  return {
+    color: isOverlapping ? 'red' : 'yellow',
+    fillOpacity: 0.5,
+  };
+}
+
+var supermarketLayer = L.geoJSON(buffers, {
+  style: styleBuffers,
+}).addTo(mymap);
+
+document.getElementById('btnTask3').addEventListener('click', function () {
+  mymap.setView([60.30129385267935, 18.410410081739627], 12);
 });
 
 // Task 4 - Raster layer - Image Overlay
